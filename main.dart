346df14263903d4166d4c113d84936ea08feb502
cjbautista22 <joybautista2005@gmail.com>
2025-08-08 17:19:48 +0800

    Understanding the Dart

---
+++ main.dart
@@ -0,0 +1,29 @@
+// void main () {
+//  dynamic name = "dart";
+//  name = 51;
+//  print ("Hello $name !");
+// }
+
+
+
+void main() {
+    double result =CalculateArea(12.5, 5, false);
+    print ('The Result for the area of rectangle is ' + result.toString());
+
+    result =CalculateArea(12.5, 5, true);
+    print ('The Result for the area of triangle is ' + result.toString());
+}
+
+double CalculateArea(double width, double height,bool isTriangle) {
+    double area;
+    if (isTriangle)
+    {
+        area = width * height / 2;
+    } else {
+            area = width * height;
+
+    }
+    area = width * height;
+    return area;
+}
+
